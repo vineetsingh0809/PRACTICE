22b5d6fda1596908d37bb0c19cb6d1ab0ac0073e
vineetsingh0809 <ankitchilma@gmail.com>
2023-11-06 20:21:16 +0530

    mobile design

--- app/navbar/Navbar.tsx
+++ app/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-full bg-purple-300 p-2">
-      <div className="w-1/3 m-auto flex items-center justify-around">
+      <div className="w-1/3 m-auto flex items-center justify-around max-[450px]:w-full">
         <Link href={"/"}>
           <h3>Home</h3>
         </Link>
