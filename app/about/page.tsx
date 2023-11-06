fac36597840e143f65e554510b606f12b6e4aa88
vineetsingh0809 <ankitchilma@gmail.com>
2023-11-06 20:18:25 +0530

    added multiple page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <div>This is About Page</div>
+    </>
+  );
+};
+
+export default page;
